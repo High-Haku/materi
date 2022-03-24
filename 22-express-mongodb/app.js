81d4776583ec24829a259fa57e9ff74373094af2
thoriqnf <thoriqnfaizal@gmail.com>
2022-03-24 10:57:30 +0700

    Adding materi mongoose

--- 22-express-mongodb/app.js
+++ 22-express-mongodb/app.js
@@ -1,54 +1,81 @@
-const express = require('express');
-const { MongoClient } = require("mongodb")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const { UserModel } = require("./models/");
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// configurasi ke mongodb
-const url = 'mongodb://localhost:27018'
-const client = new MongoClient(url);
-const dbName = 'bootcamp'
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// menghubungkan server ke mongodb
-client.connect()
-.then(() => console.log("berhasil konek ke mongodb"))
-.catch(err => console.log('gagal konek ke mongodb'))
+// configurasi ke mongodb local
+// const url = 'mongodb://localhost:27018'
+// const client = new MongoClient(url);
+// const dbName = "bootcamp";
 
-// menghubungkan ke db dan menyiapkan collection
-const db = client.db(dbName)
-const User = db.collection('user')
+const uri =
+  "mongodb+srv://skilvul:skilvul123@cluster0.wfybr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-app.use(express.json())
+mongoose
+  .connect(uri)
+  .then(() => console.log("berhasil konek ke mongoose"))
+  .catch((err) => console.log("gagal konek ke mongo"));
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json("ini dari expres yg konek yg mongodb")
-})
-
-// input data user
-app.post("/user", async (req, res) => {
-  const data = req.body
-
-  await User.insertOne(data)
-  res.json({
-    message: "berhasil input data",
-    data: 1
-  })
-})
+  res.json("ini dari expres yg konek yg mongodb atlas dengan mongoose");
+});
 
 // ambil semua data user
 app.get("/user", async (req, res) => {
-  const users = await User.find().toArray();
+  console.log(UserModel);
+  const users = await UserModel.find({});
+  console.log(users);
 
-  res.json({
-    message:"berhasil ambil data user",
-    data: users
-  })
-})
+  try {
+    res.json({
+      message: "berhasil ambil data user",
+      data: users,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
 // ambil data user by ID
+// app.get("/user/:id", async (req, res) => {
+//   const users = await User.findOne(req.params.id);
+//   res.json({
+//     message: "berhasil ambil data user",
+//     data: users,
+//   });
+// });
 
+// input data user
+// app.post("/user", async (req, res) => {
+//   const data = req.body;
 
+//   await User.insertOne(data);
+//   res.json({
+//     message: "berhasil input data",
+//     data: 1,
+//   });
+// });
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
+
+// let db = mongoose.connection;
+// menghubungkan server ke mongodb
+// client
+//   .connect()
+//   .then(() => console.log("berhasil konek ke mongodb"))
+//   .catch((err) => console.log("gagal konek ke mongodb"));
+
+// menghubungkan ke db dan menyiapkan collection
+// const db = client.db(dbName);
+// const User = db.collection("users");
